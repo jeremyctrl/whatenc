9176158c5d7b75a868f160ee419e27774b726147
Jeremy <235932977+jeremyctrl@users.noreply.github.com>
2025-10-26 01:18:00 +0300

    chore: release 0.3.0

--- whatenc/cli.py
+++ whatenc/cli.py
@@ -36,7 +36,7 @@ def print_result(line: str, top):
 def main():
     parser = argparse.ArgumentParser(prog="whatenc")
     parser.add_argument("input", help="string or path to text file")
-    parser.add_argument("-v", "--version", action="version", version="%(prog)s 0.2.1")
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s 0.3.0")
     args = parser.parse_args()
 
     print("[*] loading model")
